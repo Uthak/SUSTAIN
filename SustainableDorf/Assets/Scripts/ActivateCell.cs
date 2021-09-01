6a1467e3d9ba9afc01c6782c4b66e206b6dfc026
JanSuchanek13 <82054660+JanSuchanek13@users.noreply.github.com>
2021-09-02 00:41:08 +0200

    Update Jan
    Cell hat 2 Materialien gespeichert für den Hover Effekt
    
    bei den Maptiles den children cube.001 an zweite Stelle gesetzt, damit ActiveCell sie finden kann
    
    ActiveCell hat Hover Effekt bekommen der die überhoverte Cell und den Nachbarn andere Farben gibt

--- SustainableDorf/Assets/Scripts/ActivateCell.cs
+++ SustainableDorf/Assets/Scripts/ActivateCell.cs
@@ -16,9 +16,16 @@ public class ActivateCell : MonoBehaviour
     int sustainable;
     GameObject SceneManager;
 
+    //speicher Original Farbe
+    Color OriginalColor;
+    public Material Material1;
+    public Material Material2;
+    Renderer[] children;
+
     private void Start()
     {
         SceneManager = GameObject.Find("SceneManager");
+        
     }
 
     void OnTriggerEnter(Collider other)
@@ -38,43 +45,95 @@ public class ActivateCell : MonoBehaviour
 
     private void OnMouseOver()
     {
+        if (SceneManager.GetComponent<PlaceObjectsOnGrid>().onMousePrefab != null)
+        {
+            
+            //blau beim hovern
+            gameObject.GetComponent<Renderer>().material.color = Color.blue;
         
-        //Debug.Log(neighbors.Count);
+
+            //zählt die jeweiligen Tags der Nachbarn
+            Debug.Log(neighbors.Count);
+            foreach (var tile in neighbors)
+            {
+                children = tile.GetComponentsInChildren<Renderer>();
+                //tile.GetComponentsInChildren<>(true);
+                string str = tile.tag;
+                switch (str)
+                {
+                    case "factory":
+                        factory ++;
+                        children[1].GetComponent<Renderer>().material.color = Color.gray;//Boden von Nachbarn wird gefärbt
+
+                        break;
+                    case "social":
+                        social++;
+                        children[1].GetComponent<Renderer>().material.color = Color.magenta;
+
+                        break;
+                    case "nature":
+                        nature++;
+                        children[1].GetComponent<Renderer>().material.color = Color.green;
+
+                        break;
+                    case "sustainable":
+                        sustainable++;
+                        children[1].GetComponent<Renderer>().material.color = Color.blue;
+
+                        break;
+                    default:
+                        Debug.Log("Fehler");
+                        break;
+                }
+            }
+        
+            SceneManager.GetComponent<PlaceObjectsOnGrid>().curObject.GetComponent<Stats>().NeighborEffect(factory, social, nature, sustainable);
+            //Debug.Log("factory" + factory);
+            //Debug.Log("social" + social);
+            //Debug.Log("nature" + nature);
+            //Debug.Log("sustainable" + sustainable);
+            factory = 0;
+            social = 0;
+            nature = 0;
+            sustainable = 0;
+        }
+    }
+
+    private void OnMouseExit()
+    {
+        gameObject.GetComponent<MeshRenderer>().material = Material1;
+
         foreach (var tile in neighbors)
         {
+            children = tile.GetComponentsInChildren<Renderer>();
+            //tile.GetComponentsInChildren<>(true);
             string str = tile.tag;
             switch (str)
             {
                 case "factory":
-                    factory ++;
                     
+                    children[1].GetComponent<Renderer>().material = Material2;
+
                     break;
                 case "social":
-                    social++;
                     
+                    children[1].GetComponent<Renderer>().material = Material2;
+
                     break;
                 case "nature":
-                    nature++;
                     
+                    children[1].GetComponent<Renderer>().material = Material2;
+
                     break;
                 case "sustainable":
-                    sustainable++;
                     
+                    children[1].GetComponent<Renderer>().material = Material2;
+
                     break;
                 default:
                     Debug.Log("Fehler");
                     break;
             }
         }
-        
-        SceneManager.GetComponent<PlaceObjectsOnGrid>().curObject.GetComponent<Stats>().NeighborEffect(factory, social, nature, sustainable);
-        Debug.Log("factory" + factory);
-        Debug.Log("social" + social);
-        Debug.Log("nature" + nature);
-        Debug.Log("sustainable" + sustainable);
-        factory = 0;
-        social = 0;
-        nature = 0;
-        sustainable = 0;
     }
 }
